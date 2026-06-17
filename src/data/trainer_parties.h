a997f211f32c255456438f64c011737d1f50ddd0
Nachomatt <matthijsdell1@outlook.com>
2026-06-17 22:26:08 +0200

    Fixed Soundproof, now Tough Hide testing

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -8153,7 +8153,7 @@ static const struct TrainerMonNoItemCustomMoves sParty_Rick[] = {
     .iv = 0,
     .lvl = 4,
     .species = SPECIES_WURMPLE,
-    .moves = {MOVE_HYPER_VOICE, MOVE_SING, MOVE_GROWL, MOVE_PECK}
+    .moves = {MOVE_VITAL_THROW, MOVE_SILVER_WIND, MOVE_POUND, MOVE_PECK}
     },
     {
     .iv = 0,
