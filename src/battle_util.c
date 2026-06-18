bbf9da3cbbf12e71cebe84e3fa026281a5928758
Nachomatt <matthijsdell1@outlook.com>
2026-06-18 19:13:37 +0200

    Tough Hide fix

--- src/battle_util.c
+++ src/battle_util.c
@@ -2682,16 +2682,12 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u8 ability, u8 special, u16 moveA
                     break;
 
                 case ABILITY_TOUGH_HIDE:
-                    if (gBattleMoves[move].power < 65 || gBattleMoves[move].power > 0)
+                    if (gBattleMoves[move].power < 65 && gBattleMoves[move].power > 0)
                     {
                         if (gBattleMons[gBattlerAttacker].status2 & STATUS2_MULTIPLETURNS)
                             gHitMarker |= HITMARKER_NO_PPDEDUCT;
                         gBattlescriptCurrInstr = BattleScript_ToughHideProtected;
-                    }
-                    else
-                    {
-                        gBattlescriptCurrInstr = BattleScript_MoveHPDrain_PPLoss;
-                        effect = 1;
+                        effect = 1;      
                     }
              }       
                        
